5a4f45a522028dab1446b091a2d076693b1774ea
katrina hockman and Phil Mass <phil@>
2018-10-17 15:46:45 -0700

    add clickable name to list

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,44 @@
 $(function() {
   // Back-end logic
+  var makeClickable = function() {
+    $(".contact-name").click(function() {
+      var name = $(this).text();
+      var phone = $(this).siblings(".contact-phone").text();
+      var address = $(this).siblings(".contact-address").text();
+      var email = $(this).siblings(".contact-email").text();
 
+      $("#contact-name").text(name);
+      $("#contact-phone").text(phone);
+      $("#contact-address").text(address);
+      $("#contact-email").text(email);
+    })
+  }
 
-  // Front-end logic
-  $(".contact-name").click(function() {
-    var name = $(this).text();
-    var phone = $(this).siblings(".contact-phone").text();
-    var address = $(this).siblings(".contact-address").text();
-    var email = $(this).siblings(".contact-email").text();
+  // Front-end
+  $("#contact").submit(function(event) {
+    event.preventDefault();
+    var name = $("#name").val();
+    $("#list").append("<li><p class='contact-name'>" + name + "</p></li>");
 
-  $("#contact-name").text(name);
-  $("#contact-phone").text(phone);
-  $("#contact-address").text(address);
-  $("#contact-email").text(email);
-
+    makeClickable();
+    // <li>
+    //   <p class="contact-name">Bob</p>
+    //   <p class="contact-phone">503-123-4567</p>
+    //   <p class="contact-address">123 Oak st</p>
+    //   <p class="contact-email">bob@bob.com</p>
+    // </li>
   })
+
+  makeClickable();
+  // $(".contact-name").click(function() {
+  //   var name = $(this).text();
+  //   var phone = $(this).siblings(".contact-phone").text();
+  //   var address = $(this).siblings(".contact-address").text();
+  //   var email = $(this).siblings(".contact-email").text();
+  //
+  //   $("#contact-name").text(name);
+  //   $("#contact-phone").text(phone);
+  //   $("#contact-address").text(address);
+  //   $("#contact-email").text(email);
+  // })
 })
